bf999ea1b1a61fa26c146d4ee3b538c407de10ad
Santi Arroyo <santi.arroyo551@gmail.com>
2022-06-10 12:23:14 -0400

    commit tree c488357fe4b610a1d5b766cd9afebda2bd62de5e

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Write your solution in this file!
+var customerName = 'bob'
+
+function upperCaseCustomerName() {
+   customerName = customerName.toUpperCase();
+   //console.log(customerName);
+}
+
+function setBestCustomer () {
+    bestCustomer = 'not bob';
+   // return bestCustomer;
+}
+//console.log(setBestCustomer);
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob'
+}
+
+const leastFavoriteCustomer = 'rob';
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'ted';
+}
